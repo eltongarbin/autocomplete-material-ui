f387ea061191f3f7be2cf68e02e3aed0b552d80b
Elton Garbin <eltongarbin@gmail.com>
2018-11-09 23:07:27 -0200

    Adding material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
+import { CssBaseline, Button } from '@material-ui/core';
 
-export default () => <div>Clean Workspace</div>;
+export default () => (
+  <div>
+    <CssBaseline />
+    <Button variant="contained" color="primary">
+      Material-ui added
+    </Button>
+  </div>
+);
